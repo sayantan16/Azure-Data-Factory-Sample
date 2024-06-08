bbfb90a17ea5c385550fbba32d1f30ff4cd41362
sayantan16 <sayantankundu1611@gmail.com>
2024-06-07 21:12:27 -0400

    Change in SQL create file

--- SQL Files/Create_Table_1.sql
+++ SQL Files/Create_Table_1.sql
@@ -14,18 +14,17 @@ CREATE TABLE covid_reporting.cases_and_deaths
 )
 GO
 
-
+DROP TABLE covid_reporting.hospital_admissions_daily
 
 CREATE TABLE covid_reporting.hospital_admissions_daily
 (
-    country                 VARCHAR(100),
-    country_code_2_digit    VARCHAR(2),
-    country_code_3_digit    VARCHAR(3),
-    population              BIGINT,
-    reported_date           DATE,
-    hospital_occupancy_count BIGINT,
-    icu_occupancy_count      BIGINT,
-    source                  VARCHAR(500)
+    country                 VARCHAR(MAX),
+    indicator               VARCHAR(MAX),
+    [date]                  DATE,
+    year_week               VARCHAR(MAX),
+    [value]                 VARCHAR(MAX),
+    source                  VARCHAR(MAX),
+    [url]                   VARCHAR(MAX)
 )
 GO
 
